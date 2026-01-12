c28b12bad27c084c6987592b43af021d3a938c39
Bhupendra Yadav <bhupendrayadav2077@gmail.com>
2026-01-12 23:24:46 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -10,7 +10,14 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://mediconnects.onrender.com',
+        process.env.FRONTEND_URL
+    ].filter(Boolean),
+    credentials: true
+}));
 
 // Database Connection
 // Database Connection
